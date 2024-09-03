feb964411ab23c359e1acd79409d50d0139e477e
sandrocardozo <sandroportillo44@gmail.com>
2024-09-02 23:37:51 -0400

    Initial commit

---
+++ app/src/main/java/com/utic/accesoapp/MainActivity.java
@@ -0,0 +1,37 @@
+package com.utic.accesoapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText et1,et2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        et1=findViewById(R.id.et1);
+        et2=findViewById(R.id.et2);
+    }
+    public void verificar(View v) {
+        String clave=et2.getText().toString();
+        if (clave.length()==0) {
+            Toast notificacion= Toast.makeText(this,"La clave no puede quedar vacía",
+                    Toast.LENGTH_LONG);
+            notificacion.show();
+        }
+    }
+}
